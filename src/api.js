459e9d49f1499c7d0d3b01b15e633f948af6e97d
Jinho <dfghcvb11@naver.com>
2023-03-14 17:43:17 +0900

    feat : Sign Up API 호출

--- src/api.js
+++ src/api.js
@@ -27,4 +27,24 @@ export const verifyValidateNumber = async (email, verificationCode) => {
       console.error('generateVerificationNumber error : ' + error)
       return false;
     });
+}
+
+export const signUpNewMember = async (univId, name, password) => {
+  const payload = {
+    univId: univId,
+    name: name,
+    password: password
+  }
+
+  return await axios.post(`http://localhost:8080/member/new`, payload)
+    .then(response => {
+      console.log(response.data);
+      if (response.status === 200) {
+        return true;
+      }
+      return false;
+    }).catch(error => {
+      console.error('generateVerificationNumber error : ' + error)
+      return false;
+    });
 } 
