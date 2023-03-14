c096caa884d36b1e38b38c81b65e9bd92084311e
Jinho <dfghcvb11@naver.com>
2023-03-14 17:41:02 +0900

    feat : 이메일 인증번호 확인 api 호출

--- src/api.js
+++ src/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-export const generateVerificationNumber = async (email) => {
-  console.log(`http://localhost:8080/member/verification/${email}`);
+export const generateVerificationNumber = async email => {
   return await axios.post(`http://localhost:8080/member/verification/${email}`)
     .then(response => {
       if (response.status === 200) {
@@ -13,3 +12,19 @@ export const generateVerificationNumber = async (email) => {
       return false;
     });
 }
+
+// TODO
+export const verifyValidateNumber = async (email, verificationCode) => {
+  return await axios.post(`http://localhost:8080/member/verification/${email}/${verificationCode}`)
+    .then(response => {
+      console.log(response.data);
+      console.log(response);
+      if (response.data === 'ture') {
+        return true;
+      }
+      return false;
+    }).catch(error => {
+      console.error('generateVerificationNumber error : ' + error)
+      return false;
+    });
+} 
