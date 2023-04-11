5a2c4112d7458b3f795d67a11c7ecc2ff742e70d
binary_ho <dfghcvb11@naver.com>
2023-04-11 22:47:30 +0900

    refactor : 학생 목록을 적절하게 다듬어서 엑셀로 변환

--- src/util/xlsxConverter.js
+++ src/util/xlsxConverter.js
@@ -8,13 +8,27 @@ export const convertJsonToXlsx = (data, lectureName) => {
         return;
     }
 
-    const tableHead = [["id", "이름", "거리", "정확도", "출석 시간"]];
+    const today = moment(new Date(data[0].timestamp)).format("YYYY-MM-DD");
+
+    const studentData = [];
+    for (let i in data) {
+        const date = new Date(data[i].timestamp);
+        const timeString = date.getHours() + ':' + date.getMinutes();
+
+        studentData.push({
+            name: data[i].name,
+            id: data[i].univId,
+            distance: Math.round(Number(data[i].distance)).toFixed(0),
+            timeString: timeString
+        })
+    }
+
+    const tableHead = [["이름", "id", "거리 (m)", "출석 시간"]];
     const book = utils.book_new();
     const sheet = utils.json_to_sheet([]);
     utils.sheet_add_aoa(sheet, tableHead);
 
-    utils.sheet_add_json(sheet, data, {origin: "A2", skipHeader: true});
-    const today = moment(new Date()).format("YYYY-MM-DD");
+    utils.sheet_add_json(sheet, studentData, {origin: "A2", skipHeader: true});
     utils.book_append_sheet(book, sheet, `${today}'s Attdandance List`);
     writeFile(book, `${today} ${lectureName ? lectureName : ''} 출석 학생 목록.xlsx`);
 }
